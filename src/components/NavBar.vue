5f8d850250c30e3872ae00ffe94ad4bb97fe63ce
Kaynã Camargo <kayna30p@gmail.com>
2022-01-23 18:23:26 -0300

    solved prettier errors and started navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navbar"></div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar",
+        props: {
+            msg: String,
+        },
+    };
+</script>
+
+<style>
+    .navbar {
+        position: absolute;
+        top: 0;
+        height: 30px;
+        background-color: #dde7ee;
+        width: 100%;
+    }
+</style>
